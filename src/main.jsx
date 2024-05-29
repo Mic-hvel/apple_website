12b2030c395813c96ec8d256d3833e5509b40636
Mic-hvel <o.temitopemichael@gmail.com>
2024-05-29 19:43:42 +0100

    Adding the Features and Footer section

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ Sentry.init({
   dsn: "https://d674932a77e6d9b9ced1190d70fd4691@o4506876178464768.ingest.us.sentry.io/4506876181151744",
   integrations: [
     Sentry.browserTracingIntegration(),
-    Sentry.metrics.metricsAggregatorIntegration(),
+    // Sentry.metrics.metricsAggregatorIntegration(),
     Sentry.reactRouterV6BrowserTracingIntegration({
       useEffect: React.useEffect,
     }),
@@ -29,3 +29,5 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </React.StrictMode>
 );
+
+export default Sentry.withProfiler(App);
