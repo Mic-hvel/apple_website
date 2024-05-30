e8eba8a68b10c8d635551ea361371783009f427c
Mic-hvel <o.temitopemichael@gmail.com>
2024-05-30 23:58:02 +0100

    small changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = () => {
             you.
           </p>
           <p className="font-semibold text-gray text-xs">
-            Or call 000800-040-1966
+            Or call 00800-040-1966
           </p>
         </div>
 
